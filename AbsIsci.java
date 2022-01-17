2934dcb48daaf627ff3a0fe4ea2d049f82349797
Fkoni <93380100+Fkoni@users.noreply.github.com>
2022-01-17 13:30:18 +0300

    Add files via upload

---
+++ AbsIsci.java
@@ -0,0 +1,7 @@
+package day42_AbstractClasses;
+
+public abstract class AbsIsci extends Muhasebe{
+	
+	public abstract void mesai(); 
+
+}
---
+++ Personel.java
@@ -0,0 +1,15 @@
+package day42_AbstractClasses;
+
+public class Personel {
+
+
+	public void statu() {
+		System.out.println("Sirkete alinan herkes bizim personelimizdir .. imza:Persdonel");
+	}
+
+	public void abstractMethod() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
